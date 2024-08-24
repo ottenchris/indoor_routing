f920b4f0acf5560be08de577778e12d8f901d66a
Talion <44683039+talionderhobbit@users.noreply.github.com>
2024-08-24 02:33:07 +0200

    Disable debug banner in the MaterialApp widget in app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:design_system_flutter/design_system_flutter.dart' as dsf;
+import 'package:flutter/material.dart';
 import 'package:indoor_routing/pres/home_page.dart';
-import 'pres/services_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: dsf.SBBTheme.dark(),
       home: const HomePage(),
     );
